82184865bc9a1d735e0b226fda829b4a8f7e5eb3
ValGillet <val.gillet12@gmail.com>
2025-04-25 10:52:23 +0200

    improvements estimate_transform_sift

--- utils/offsets.py
+++ utils/offsets.py
@@ -25,18 +25,23 @@ def xy_offset_to_pad(offset):
     return pad
 
 
-def estimate_transform_sift(img1, img2, scale=1, return_homography=False):
+def estimate_transform_sift(img1, img2, scale=1, refine_estimate=True):
 
     '''
     Estimate transformation (xy offset and rotation) from img2 to img1
+    Returns offset in pixels, rotation before transform, whether estimate is robust based on number and proportion of matches used
     '''
+
+    # knnMatch will return an error if there are too many keypoints so we limit their number
+    # see
+    max_features=250000
 
     # Downsample images for faster computations
     ds_img1 = downsample(img1, scale)
     ds_img2 = downsample(img2, scale)
 
     # Find keypoints using SIFT
-    sift = cv2.SIFT_create()
+    sift = cv2.SIFT_create(nfeatures=max_features)
     kp1, des1 = sift.detectAndCompute(ds_img1,None)
     kp2, des2 = sift.detectAndCompute(ds_img2,None)
 
@@ -45,140 +50,42 @@ def estimate_transform_sift(img1, img2, scale=1, return_homography=False):
     bf = cv2.BFMatcher()
     matches = bf.knnMatch(des1,des2,k=2)
 
-    good = []
+    good_matches = []
     for m,n in matches:
         if m.distance < 0.7*n.distance:
-            good.append(m)
+            good_matches.append(m)
 
-    src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
-    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+    src_pts = np.float32([kp1[m.queryIdx].pt for m in good_matches]).reshape(-1,1,2)
+    dst_pts = np.float32([kp2[m.trainIdx].pt for m in good_matches]).reshape(-1,1,2)
 
     # Estimate affine transformation matrix
     try:
-        M, _ = cv2.estimateAffinePartial2D(src_pts, dst_pts)    
-    except:
-        raise RuntimeError('Error while estimating homography. There may not be enough keypoints.')
-    if M is None:
-        raise ValueError('Error while estimating homography. Keypoints may not correspond between images')
+        M, inliers = cv2.estimateAffinePartial2D(src_pts, dst_pts)
+    except cv2.error as e:
+        if 'count >= 0' in e.err:
+            M = None
+        else:
+            raise e
+    except Exception as e:
+        raise e
+               
+    if M is not None:
+        # Extract translation offsets, divide by scale so it matches initial image resolution
+        xy_offset = M[:, 2] / scale
 
-    # Extract translation offsets
-    xy_offset = M[:, 2]
+        # Extract rotation angle in degrees
+        theta = np.degrees(np.arctan2(M[1, 0], M[0, 0]))
 
-    # Extract rotation angle in degrees
-    theta = np.degrees(np.arctan2(M[1, 0], M[0, 0]))
-    
-    # Offset in pixels, rotation before transform
-    if return_homography:
-        return M, xy_offset/scale, theta
+        robust_estimate = (len(good_matches)>10) and (inliers.sum() / len(good_matches) > 0.5)
     else:
-        return xy_offset/scale, theta
+        xy_offset = None
+        theta = None
+        robust_estimate = False
 
-
-def estimate_tiles_overlap(img1, 
-                           img2, 
-                           prelim_overlap,
-                           axis, 
-                           scale,
-                           score_threshold=0.8):
-    from emalign.utils.tile_map_positions import check_overlap
-    
-    if axis == 0:
-        crop_img1 = img1[-prelim_overlap:, :]
-        crop_img2 = img2[:prelim_overlap, :]
-    elif axis == 1:
-        crop_img1 = img1[:, -prelim_overlap:]
-        crop_img2 = img2[:, :prelim_overlap]
-
-    # First, try increasing only compute scale
-    try:
-        offset, angle = estimate_transform_sift(crop_img1, crop_img2, scale[0])
-        overlap_score = check_overlap(crop_img1, crop_img2, offset, angle, threshold=score_threshold, refine=True)            
-    except:
-        # There may not be enough keypoints to compute offset
-        overlap_score = 0
-
-    if overlap_score < score_threshold:
-        # Decrease downsampling
-        try:
-            offset, angle = estimate_transform_sift(crop_img1, crop_img2, scale[1])
-            overlap_score = check_overlap(crop_img1, crop_img2, offset, angle, threshold=score_threshold, refine=True)
-        except:
-            overlap_score = 0
-    
-    if overlap_score > score_threshold:
-        overlap = prelim_overlap - np.abs(offset[::-1][axis])
-        return offset, overlap, overlap_score
-    
-    # Second, try increasing overlap
-    max_overlap = max(img1.shape[axis], img2.shape[axis])
-    prelim_overlap = max_overlap // 2
-
-    if axis == 0:
-        crop_img1 = img1[-prelim_overlap:, :]
-        crop_img2 = img2[:prelim_overlap, :]
-    elif axis == 1:
-        crop_img1 = img1[:, -prelim_overlap:]
-        crop_img2 = img2[:, :prelim_overlap]
-
-    try:
-        offset, angle = estimate_transform_sift(crop_img1, crop_img2, scale[0])
-        overlap_score = check_overlap(crop_img1, crop_img2, offset, angle, threshold=score_threshold, refine=True)            
-    except:
-        # There may not be enough keypoints to compute offset
-        overlap_score = 0
-
-    if overlap_score < score_threshold:
-        # Decrease downsampling
-        try:
-            offset, angle = estimate_transform_sift(crop_img1, crop_img2, scale[1])
-            overlap_score = check_overlap(crop_img1, crop_img2, offset, angle, threshold=score_threshold, refine=True)
-        except:
-            overlap_score = 0
-            offset = 0
-
-    overlap = prelim_overlap - np.abs(offset[::-1][axis]) if overlap_score > score_threshold else 0
-    return offset, overlap, overlap_score
-
-
-def estimate_tilemap_overlap(tile_space,
-                             tile_map,
-                             prelim_overlap=500,
-                             scale=[0.5,1],
-                             score_threshold=0.8):
-    
-    '''
-    Estimate the overlap between tiles of a tile_map. 
-    Using SIFT for estimate because more consistent results.
-    '''
-    
-    overlaps_x = []
-    for x in range(0, tile_space[1] - 1):
-        for y in range(0, tile_space[0]):
-            left = tile_map[(x,y)] 
-            right = tile_map[(x+1,y)] 
-
-            _, overlap, _ = estimate_tiles_overlap(left, 
-                                                       right, 
-                                                       prelim_overlap=prelim_overlap,
-                                                       axis=1, 
-                                                       scale=scale,
-                                                       score_threshold=score_threshold)
-            overlaps_x.append(overlap)
-
-    overlaps_y = []
-    for y in range(0, tile_space[0] - 1):
-        for x in range(0, tile_space[1]):
-            bot = tile_map[(x,y)] 
-            top = tile_map[(x,y+1)] 
-            
-            _, overlap, _ = estimate_tiles_overlap(bot, 
-                                                   top, 
-                                                   prelim_overlap=prelim_overlap,
-                                                   axis=0, 
-                                                   scale=scale,
-                                                   score_threshold=score_threshold)
-            overlaps_y.append(overlap)
-    return int(np.max(overlaps_x + overlaps_y))
+    if refine_estimate and not robust_estimate and scale<0.9:
+        return estimate_transform_sift(img1, img2, scale=scale+0.1, refine_estimate=False)
+    else:
+        return xy_offset, theta, robust_estimate
 
 
 def estimate_rough_z_offset(img1, img2, scale=0.1, range_limit=10, filter_size=5, patch_factor=1): 
