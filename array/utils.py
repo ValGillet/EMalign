080fff04520742765b5a4c39b0091f2714cf04f9
ValGillet <val.gillet12@gmail.com>
2025-05-07 17:01:58 +0200

    added functions for computing image sharpness

--- array/utils.py
+++ array/utils.py
@@ -1,8 +1,8 @@
 import cv2
-import logging
 import numpy as np
 
 
+# PAD
 def xy_offset_to_pad(offset):
     pad = np.zeros([2,2], dtype=int)
     x,y = [int(i) for i in offset]
@@ -44,22 +44,56 @@ def homogenize_arrays_shape(arrs):
     return [pad_to_shape(a, max_shape) for a in arrs]
 
 
-def compute_laplacian_var_diff(overlap_1, 
-                               overlap_2, 
-                               mask):
+# ASSESS QUALITY
+def _compute_laplacian_var(arr, mask=None):
+    '''
+    Compute laplacian variance. Provides an indication of image sharpness but is sensitive to contrast.
+    '''
+    if mask is not None:
+        l = cv2.Laplacian(arr, cv2.CV_64F)[mask]
+    else:
+        l = cv2.Laplacian(arr, cv2.CV_64F)
+    return np.var(l)
+
+
+def _compute_sobel_mean(arr, mask=None):
+    '''
+    Apply Sobel operator. Provides an indication of image sharpness
+    '''
+    if mask is not None:
+        sobel_x = cv2.Sobel(arr, cv2.CV_64F, 1, 0, ksize=5)[mask]
+        sobel_y = cv2.Sobel(arr, cv2.CV_64F, 0, 1, ksize=5)[mask]
+    else:
+        sobel_x = cv2.Sobel(arr, cv2.CV_64F, 1, 0, ksize=5)
+        sobel_y = cv2.Sobel(arr, cv2.CV_64F, 0, 1, ksize=5)
+    sobel = np.sqrt(sobel_x**2 + sobel_y**2)
+    return np.mean(sobel)
+
+
+def _compute_grad_mag(arr, mask=None):
+    '''
+    Compute gradient magnitude. Provides an indication of pixel variation, which can be used to measure sharpness/quality. 
+    '''
+    gy, gx = np.gradient(arr)
+
+    if mask is not None:
+        gnorm = np.sqrt(gx**2 + gy**2)[mask]
+    else:
+        gnorm = np.sqrt(gx**2 + gy**2)
+    return np.average(gnorm)
+
+
+def compute_laplacian_var_diff(overlap1, 
+                               overlap2, 
+                               mask=None):
 
     '''
     Compute a metric ([0,1]) describing how well two arrays overlap, based on laplacian filter.
     If score is 1, overlapping regions have the same edge content and therefore overlap well.
     '''
     
-    mask = np.ones_like(overlap_1).astype(bool) if mask is None else mask
-
-    laplacian1 = cv2.Laplacian(overlap_1, cv2.CV_64F)[mask]
-    laplacian2 = cv2.Laplacian(overlap_2, cv2.CV_64F)[mask]
-
-    lap_var1 = np.var(laplacian1)
-    lap_var2 = np.var(laplacian2)
+    lap_var1 = _compute_laplacian_var(overlap1, mask)
+    lap_var2 = _compute_laplacian_var(overlap2, mask)
 
     # Calculate an index of difference in edge content (variance of laplacian)
     # Between 0 and 1, low means exact same content, 1 means different
